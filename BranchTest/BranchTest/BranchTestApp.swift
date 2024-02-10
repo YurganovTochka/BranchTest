057fbcf1f0599f0a1137edd50183bdf7eac4b681
Dmitriy Yurganov <iurganov@tochka.com>
2024-02-10 12:42:08 +0700

    init

---
+++ BranchTest/BranchTest/BranchTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BranchTestApp.swift
+//  BranchTest
+//
+//  Created by Дмитрий Юрганов on 10.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct BranchTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
